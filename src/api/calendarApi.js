37808600c7c6edf8a8418ed0bb09ac843aed9e8a
raularmasbx@gmail.com <raularmasbx@gmail.com>
2024-06-29 03:50:35 -0500

    updates console.log('VITE_API_URL:', VITE_API_URL); // Verificar el valor de VITE_API_URL

--- src/api/calendarApi.js
+++ src/api/calendarApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getEnvVariables } from '../helpers';
 
 const { VITE_API_URL } = getEnvVariables()
-
+console.log('VITE_API_URL:', VITE_API_URL); // Verificar el valor de VITE_API_URL
 
 const calendarApi = axios.create({
     baseURL: VITE_API_URL
